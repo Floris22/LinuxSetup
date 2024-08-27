cdf5cac1877d895361cf8f77a31fe2d00e0ee133
Floris Buyse <91726746+Floris22@users.noreply.github.com>
2024-08-27 18:39:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,28 @@
 
 # Update package database and upgrade system
 echo "Updating and upgrading the system..."
+echo
+
 sudo pacman -Syy --noconfirm
 sudo pacman -S archlinux-keyring --noconfirm
 sudo pacman -Syu --noconfirm
 
 echo "System is up to date!"
+echo
 
 #####################
 # Download packages #
 #####################
+echo
 echo "Downloading packages..."
+echo
+
 sudo pacman -S vim timeshift python-virtualenv zsh unzip curl fuse2 wget vlc ufw obsidian htop fastfetch discord android-tools libreoffice-fresh docker docker-compose docker-buildx --noconfirm
 # with confirm because certain choices have to be made
 sudo pacman -S qemu virt-manager virt-viewer dnsmasq bridge-utils dmidecode
-echo "Packages downloaded\n\n"
+echo "Packages downloaded"
+echo
+echo
 
 ###########################################
 # Give permissions for docker and libvirt #
@@ -28,32 +36,50 @@ echo "Permissions set"
 ##############################
 # Downloading Jetbrains mono #
 ##############################
-echo "\n\nDownloading jetbrains mono font\n"
+echo
+echo
+echo "Downloading jetbrains mono font"
+echo
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
-echo "Jetbrains mono setup complete\n\n"
+echo "Jetbrains mono setup complete"
+echo
+echo
 
 #############################################
 # Setting up terminal with starship and zsh #
 #############################################
-echo "Setting up terminal\n\n"
-echo "Downloading starship\n"
+echo "Setting up terminal"
+echo
+echo "Downloading starship"
+echo
+
 sudo curl -sS https://starship.rs/install.sh | sh
 
-echo "Starship downloaded, moving files from github to the right directories...\n"
+echo "Starship downloaded, moving files from github to the right directories..."
+echo
+
 mv ~/LinuxSetup/.zshrc ..
 mv ~/LinuxSetup/starship.toml ~/.config/
 
-echo "Installing autosuggestions for zsh\n"
+echo "Installing autosuggestions for zsh"
+echo
+
 mkdir ~/.config/.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/.zsh/zsh-autosuggestions
 
-echo "Setting zsh as default\n"
+echo "Setting zsh as default"
+echo
 chsh
 
 ################
 # Setup cursor #
 ################
-echo "\n\nSetting up cursor\n\n"
+echo
+echo
+echo "Setting up cursor"
+echo
+echo
 
 mkdir -p ~/AppImages/cursor
 mkdir -p ~/.local/share/applications
@@ -77,11 +103,33 @@ EOL
 chmod +x "$CURSOR_DESKTOP_FILE"
 chmod +x ~/AppImages/cursor/cursor.AppImage
 echo "Cursor setup complete"
+echo
+echo
+
+#############
+# YTD Setup #
+#############
+echo "Setting up ytd"
+echo
+echo
+
+mkdir -p ~/AppImages/ytd
+mv ./ytd/* ~/AppImages/ytd
+cd ~/AppImages/ytd
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+pip3 install -r requirements.txt
+
+deactivate
+echo "Ytd setup complete"
 
 ##########################################################################
 # Set battery threshold service so that battery is only charged till 60% #
 ##########################################################################
 echo "Setting up threshold service for battery"
+echo
 
 BATTERY_NAME=$(ls /sys/class/power_supply | grep BAT)
 
@@ -129,22 +177,3 @@ echo "Battery status: $BATTERY_STATUS"
 echo "To apply changes to the threshold, edit the service file at $SERVICE_FILE and restart the service with:"
 echo "sudo systemctl daemon-reload"
 echo "sudo systemctl restart battery-charge-threshold.service"
-
-#############
-# YTD Setup #
-#############
-echo "Setting up ytd"
-
-mkdir -p ~/AppImages/ytd
-mv ./ytd/* ~/AppImages/ytd
-cd ~/AppImages/ytd
-
-python3 -m venv .venv
-
-source .venv/bin/activate
-pip3 install -r requirements.txt
-
-deactivate
-echo "Ytd setup complete"
-
-
