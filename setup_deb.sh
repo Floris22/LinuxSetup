203e79e29ca8b045ade16baf84ca34bfbbde6b93
Floris Buyse <91726746+Floris22@users.noreply.github.com>
2024-10-04 17:05:42 +0200

    Update setup_deb.sh

--- setup_deb.sh
+++ setup_deb.sh
@@ -83,46 +83,22 @@ sudo curl -sS https://starship.rs/install.sh | sh
 echo_custom "Adding custom functions and eval to .bashrc"
 cat >> ~/.bashrc <<EOL
 cd() {
-    builtin cd "$@" && ls -lh --color
+    builtin cd "\$@" && ls -lh --color
 }
 
 # create dir and cd into it
 mkcd() {
-    mkdir -p "$1" && cd "$1"
-}
-
-# extract func
-extract() {
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2)   tar xjf $1     ;;
-            *.tar.gz)    tar xzf $1     ;;
-            *.bz2)       bunzip2 $1     ;;
-            *.rar)       unrar e $1     ;;
-            *.gz)        gunzip $1      ;;
-            *.tar)       tar xf $1      ;;
-            *.tbz2)      tar xjf $1     ;;
-            *.tgz)       tar xzf $1     ;;
-            *.zip)       unzip $1       ;;
-            *.Z)         uncompress $1  ;;
-            *.7z)        7z x $1        ;;
-            *)           echo "'$1' cannot be extracted via extract()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
+    mkdir -p "\$1" && cd "\$1"
 }
 
 # find file using pattern
 ff() {
-    find . -type f -iname '*'"$*"'*' -ls
+    find . -type f -iname '*"\$*"'*' -ls
 }
 
 # Aliases
 alias ls='ls --color'
 alias ytd=~/Applications/ytd/yt.sh
-
-# Exports
 EOL
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
